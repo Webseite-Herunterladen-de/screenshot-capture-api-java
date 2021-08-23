3189ba9fc7faf2929de33947118bd2f772968a77
Roman Kobosil <roman@kobosil.me>
2021-08-23 19:25:22 +0200

    New client from openapi-generator-cli

--- src/main/java/io/swagger/client/auth/ApiKeyAuth.java
+++ src/main/java/io/swagger/client/auth/ApiKeyAuth.java
@@ -17,7 +17,7 @@ import io.swagger.client.Pair;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-08-23T19:13:32.449+02:00[Europe/Berlin]")public class ApiKeyAuth implements Authentication {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-08-23T19:25:18.879+02:00[Europe/Berlin]")public class ApiKeyAuth implements Authentication {
   private final String location;
   private final String paramName;
 
