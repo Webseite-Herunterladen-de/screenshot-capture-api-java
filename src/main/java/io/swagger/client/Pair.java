db9fed6d95ec5a46bdb12ca449d185ca84219663
Roman Kobosil <roman@kobosil.me>
2021-09-20 16:23:20 +0200

    New client from openapi-generator-cli

--- src/main/java/io/swagger/client/Pair.java
+++ src/main/java/io/swagger/client/Pair.java
@@ -12,7 +12,7 @@
 
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-09-14T20:57:16.035+02:00[Europe/Berlin]")public class Pair {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-09-20T16:23:19.561+02:00[Europe/Berlin]")public class Pair {
     private String name = "";
     private String value = "";
 
--- src/main/java/io/swagger/client/auth/OAuth.java
+++ src/main/java/io/swagger/client/auth/OAuth.java
@@ -17,7 +17,7 @@ import io.swagger.client.Pair;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-09-14T20:57:16.035+02:00[Europe/Berlin]")public class OAuth implements Authentication {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-09-20T16:23:19.561+02:00[Europe/Berlin]")public class OAuth implements Authentication {
   private String accessToken;
 
   public String getAccessToken() {
